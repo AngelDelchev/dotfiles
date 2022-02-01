418a1da1ffcb903776d511bbd8e90ad40fcb41e8
JackBlackAD <95856839+JackBlackAD@users.noreply.github.com>
2022-02-01 04:05:04 +0200

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash 
+
+lxsession &
+picom &
+/usr/bin/emacs --daemon &
+volumeicon &
+nm-applet &
+nitrogen --restore &
---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+### RANDOM COLOR SCRIPT ###
+colorscript random
+
